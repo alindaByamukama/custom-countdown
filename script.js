55ec6324b1fbacecc5b19263cadd9b1bb272b463
AlindaB <alindabyamukama@gmail.com>
2022-03-29 14:36:47 +0300

    JS: created function that takes title and date values from form and submits them

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const inputContainer = document.getElementById('input-container')
-const countdownFOrm = document.getElementById('countdownForm')
+const countdownForm = document.getElementById('countdownForm')
 const dateEL = document.getElementById('date-picker')
+
+let countdownTitle = ' '
+let coutndownDate = ' '
 
 // set date min with todays date
 const today = new Date().toISOString().split('T')[0]
-console.log(today)
+dateEL.setAttribute('min', today)
+
+// take values from Form input
+function updateCountdown(e) {
+    e.preventDefault()
+    countdownTitle = e.srcElement[0].value
+    coutndownDate = e.srcElement[1].value
+
+    console.log(countdownTitle, coutndownDate)
+}
+
+// event listeners
+countdownForm.addEventListener('submit', updateCountdown)
